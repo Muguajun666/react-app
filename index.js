962395d1e42d1af05821a64a61bca4f5c4dc5df8
liushuli <liushulialen@qq.com>
2024-10-29 20:04:29 +0800

    fix: 修改格式

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './app/index';
-import {name as appName} from './app.json';
-import "./global.css"
+import { AppRegistry } from 'react-native'
+import App from './app/index'
+import { name as appName } from './app.json'
+import './global.css'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
